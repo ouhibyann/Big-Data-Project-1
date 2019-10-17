7f1d91dacea48dce0a73b784656dfbf115e620a8
yann ouhib <yannis.ouhib@etu.u-pec.fr>
2019-10-17 23:13:52 +0200

    Cleaning data

--- Scrapping computer science bibliographies.py
+++ Scrapping computer science bibliographies.py
@@ -8,11 +8,19 @@ Soup = BeautifulSoup(Page, "html.parser")
 # Get the first article + removing tags
 docs_with_tags = Soup.find('pre', attrs={'class':'bibtex'})
 docs = docs_with_tags.text.strip()
-print(docs)
 
 
-'''
-# Writing it in file
-file1 = open("Myfile.txt", "a")
+# Writing it in file and cleaning it -without the '@Article'.
+file1 = open("Myfile.txt", "w+")
+clean_file1 = open("cleaned_file1.txt", "w+")
+lst = []
+file1.truncate(0)
 file1.write(docs)
-'''
+for line in docs:
+    lst.append(line)
+del lst[0:8]
+# print(lst)
+file1.close()
+for i in range(len(lst)):
+    clean_file1.write(lst[i])
+clean_file1.close()
