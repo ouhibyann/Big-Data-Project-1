065368d5c78a9df87b7dee703d04b89b0e3123cc
yann ouhib <yannis.ouhib@etu.u-pec.fr>
2019-10-17 21:52:11 +0200

    Modification to add the proper URL from the resulting query "global warming"

--- Scrapping computer science bibliographies.py
+++ Scrapping computer science bibliographies.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-URL_page = "https://liinwww.ira.uka.de/bibliography/index.html"
+URL_page = "http://liinwww.ira.uka.de/csbib?query=%2b%28ti:global%20ti:warming%29%20%2byr:%5b2013%20TO%209999%5d&sort=score&results=bibtex"
 Page = urlopen(URL_page)
 Soup = BeautifulSoup(Page, "html.parser")
 
