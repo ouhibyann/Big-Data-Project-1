9a3aa891355872ae186422060605e5afc688e799
yann ouhib <yannis.ouhib@etu.u-pec.fr>
2019-12-06 18:44:00 +0100

    Cleaning data and extracting first name letter associated to an author

--- graphCreate.py
+++ graphCreate.py
@@ -6,17 +6,26 @@ import igraph
 with open('savedrecs-2.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
     line_count = 0
-    lst_Full_Name = []
     file1 = open("Nodes_clean.txt", "w+")
     for row in csv_reader:
-        lst_Full_Name.append([])
-        print(row)
+        #print(row)
         for i in row:
             if i != '' and i != '\t':
                 file1.write(i)
-                # The type of Name + first name is as followed :
-                full_Name = re.match("[a-zA-Z0-9_], [a-zA-Z0-9_]", i)
-                lst_Full_Name[line_count].append(full_Name)
         file1.write('\n')
         line_count += 1
     print("Number of nodes is : " + str(line_count))
+    file1.close()
+
+    #This part extracts all the first name letters
+    #In order to latter obtain a single list with the author name associated with first name
+    file2 = open("Nodes_clean.txt", 'r')
+    lst_First_Name = []
+    count = 0
+    for row in file2:
+        #lst_First_Name.append([])
+        match = re.findall(" [a-zA-Z] | [a-zA-Z]", row)
+        lst_First_Name.append(match)
+        count += 1
+    print(lst_First_Name)
+
